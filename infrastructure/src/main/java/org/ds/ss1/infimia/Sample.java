6caa02c9bef904a7c38244aaa52e2315b02fbd49
d-smith <doug.smith.mail@gmail.com>
2022-03-27 09:45:58 -0700

    some refactoring, playing around...

--- infrastructure/src/main/java/org/ds/ss1/infimia/Sample.java
+++ infrastructure/src/main/java/org/ds/ss1/infimia/Sample.java
@@ -17,6 +17,7 @@ public class Sample {
 
         @Override
         public void saveFragment(List<String> fragment) {
+            System.out.println(String.format("save fragment %s", fragment.toString()));
             Collections.sort(fragment);
             store.add(fragment.toString());
         }
@@ -28,17 +29,42 @@ public class Sample {
         }
     }
 
-    public static void main(String... args) throws Exception {
-        String[] endpoints = new String[] { "A", "B", "C", "D", "E","F","G","H" };
+    public static StatefulSearchingShuffleSharder<String> createSharder(String[] endpoints) {
         SingleCellLattice<String> lattice = new SingleCellLattice<String>();
         lattice.addEndpoints(Arrays.asList(endpoints));
 
         MockFragmentStore mockStore = new MockFragmentStore();
         StatefulSearchingShuffleSharder<String> sharder = new StatefulSearchingShuffleSharder<String>(mockStore);
-        for(int i = 0; i < 28; i++) {
-            Lattice<String> l = sharder.shuffleShard(lattice,2,1);
-            System.out.println(l.toString());
+
+        return sharder;
+    }
+
+    public static Lattice<String> createLattice(String[] endpoints) {
+        SingleCellLattice<String> lattice = new SingleCellLattice<String>();
+        lattice.addEndpoints(Arrays.asList(endpoints));
+        return lattice;
+    }
+
+    public static void main(String... args) throws Exception {
+        String[] endpoints = new String[] { "A", "B", "C", "D", "E","F","G","H","I","J","K","L" };
+
+
+        Lattice<String> lattice = createLattice(endpoints);
+        StatefulSearchingShuffleSharder<String> sharder = createSharder(
+            endpoints
+        );
+
+        for(int i = 0; i < 1000; i++) {
+            try {
+                Lattice<String> l = sharder.shuffleShard(lattice, 2, 1);
+                System.out.println(l.toString());
+            } catch(NoShardsAvailableException e) {
+                System.out.println("No more shards available - allocated " + i);
+                break;
+            }
         }
+
+
 
     }
 }
